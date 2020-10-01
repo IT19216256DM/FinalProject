4970e3ee281dfe96b3fd7f89a0c1a2eb373d34d3
Dell <dewmini970915@gmail.com>
2020-10-01 12:45:01 +0530

    Mad Final Project 08

---
+++ app/src/main/java/com/example/finalproject/Login.java
@@ -0,0 +1,28 @@
+package com.example.finalproject;
+
+public class Login {
+
+        private String txtUser;
+        private String txtPass;
+
+        public Login() {
+        }
+
+        public String getTxtUser() {
+            return txtUser;
+        }
+
+        public void setTxtUser(String txtUser) {
+            this.txtUser = txtUser;
+        }
+
+        public String getTxtPass() {
+            return txtPass;
+        }
+
+        public void setTxtPass(String txtPass) {
+            this.txtPass = txtPass;
+        }
+    }
+
+
