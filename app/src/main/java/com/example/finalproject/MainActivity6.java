b9e3dd4ecbb527a078b03e16503e3c899d8f3c34
Dell <dewmini970915@gmail.com>
2020-10-01 12:49:14 +0530

    Mad Final Project 11

--- app/src/main/java/com/example/finalproject/MainActivity6.java
+++ app/src/main/java/com/example/finalproject/MainActivity6.java
@@ -3,8 +3,17 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity6 extends AppCompatActivity {
+    EditText txtUser, txtPass;
+    Button btnlog;
+    DatabaseReference dbRef;
+    Login log;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
